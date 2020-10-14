b6350f0d5709fe8ad898487674aa88a215bc62b9
lucasbnunes <lucasbnunes23@gmail.com>
2020-10-14 12:56:26 -0300

    Correção da navbar

---
+++ Sistema/Scripts/script.js
@@ -0,0 +1,15 @@
+﻿let timer = null;
+
+window.onscroll = () => {
+    clearTimeout(timer);
+    timer = setTimeout(() => {
+        console.log(window.scrollY);
+        if (window.scrollY > window.innerHeight * 0.26) {
+            document.querySelector('.navbar').classList.add('navbar-light');
+            console.log(window.scrollY);
+        } else {
+            document.querySelector('.navbar').classList.remove('navbar-light');
+        }
+    }, 100);
+    
+};
