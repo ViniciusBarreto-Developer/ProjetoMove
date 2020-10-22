d7c5601db141c4a79ebb1abd66b036b62ad08791
lucasbnunes <lucasbnunes23@gmail.com>
2020-10-22 00:46:00 -0300

    Botão de voltar para o topo FIX

--- Sistema/Scripts/script.js
+++ Sistema/Scripts/script.js
@@ -1,15 +1,32 @@
 ﻿let timer = null;
 
-window.onscroll = () => {
-    clearTimeout(timer);
-    timer = setTimeout(() => {
-        console.log(window.scrollY);
-        if (window.scrollY > window.innerHeight * 0.26) {
-            document.querySelector('.navbar').classList.add('navbar-light');
-            console.log(window.scrollY);
-        } else {
-            document.querySelector('.navbar').classList.remove('navbar-light');
-        }
-    }, 100);
-    
-};
+//window.onscroll = () => {
+//    clearTimeout(timer);
+//    timer = setTimeout(() => {
+//        console.log(window.scrollY);
+//        if (window.scrollY > window.innerHeight * 0.26) {
+//            document.querySelector('.navbar').classList.add('navbar-light');
+//            console.log(window.scrollY);
+//        } else {
+//            document.querySelector('.navbar').classList.remove('navbar-light');
+//        }
+//    }, 100);
+
+//};
+
+document.onreadystatechange = () => {
+    window.onscroll = () => {
+        clearTimeout(timer);
+        timer = setTimeout(() => {
+            if (window.innerWidth > 768) {
+                if (window.scrollY > window.innerHeight * 0.2) {
+                    document.querySelector('.back-to-top').classList.remove('hidden');
+                } else {
+                    console.log(window.scrollY);
+                    document.querySelector('.back-to-top').classList.add('hidden');
+                }
+            }
+        }, 500);
+
+    };
+}
