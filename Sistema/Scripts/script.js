d1df2df7fcd9210abcdc5b5b45e50087d6257a95
lucasbnunes <lucasbnunes23@gmail.com>
2020-10-25 01:30:32 -0300

    Modais funcionando

--- Sistema/Scripts/script.js
+++ Sistema/Scripts/script.js
@@ -1,6 +1,52 @@
-﻿let timer = null;
+﻿
+function modalBehavior() {
+    const openModalBtns = document.querySelectorAll('[data-open-modal]');
 
-document.onreadystatechange = () => {
+    if (openModalBtns !== null) {
+
+        function openModal(targetId) {
+            return () => {
+                document.body.style.overflow = 'hidden';
+                document.querySelector(`#${targetId}`).classList.remove('hidden-animated');
+            }
+        }
+
+        function closeModal(targetId) {
+            return () => {
+                document.body.style.overflow = 'visible';
+                document.querySelector(`#${targetId}`).classList.add('hidden-animated');
+            }
+        }
+
+        function outsideClick(e, modal) {
+            if (e.target === modal) {
+                modal.classList.add('hidden-animated');
+                document.body.style.overflow = 'visible';
+            }
+        }
+
+        const closeModalBtns = document.querySelectorAll('[data-close-modal]');
+        const allModals = document.querySelectorAll('.modal-bg');
+
+        openModalBtns.forEach((item) => {
+            item.addEventListener('click', openModal(item.dataset.openModal));
+        });
+
+        closeModalBtns.forEach((item) => {
+            item.addEventListener('click', closeModal(item.dataset.closeModal));
+        });
+
+        allModals.forEach((item) => {
+            item.addEventListener('click', (e) => {
+                outsideClick(e, item)
+            });
+        });
+    }
+}
+
+let timer = null;
+
+document.addEventListener('DOMContentLoaded', () => {
     window.onscroll = () => {
         clearTimeout(timer);
         timer = setTimeout(() => {
@@ -8,11 +54,12 @@ document.onreadystatechange = () => {
                 if (window.scrollY > window.innerHeight * 0.2) {
                     document.querySelector('.back-to-top').classList.remove('hidden');
                 } else {
-                    console.log(window.scrollY);
                     document.querySelector('.back-to-top').classList.add('hidden');
                 }
             }
         }, 500);
     };
-}
+
+    modalBehavior();
+});
 
